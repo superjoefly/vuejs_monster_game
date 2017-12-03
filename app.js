7cb424020238cb950a9e7018c05092852665481a
superjoefly <superjoefly@gmail.com>
2017-12-02 18:47:39 -0700

    Added image uploader and changed some styles

--- app.js
+++ app.js
@@ -4,10 +4,33 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
-    turns: []
+    turns: [],
+    image: ''
   },
 
   methods: {
+    // Image Upload
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    },
+    // removeImage(e) {
+    //   this.image = '';
+    // },
+
+
     startGame() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
